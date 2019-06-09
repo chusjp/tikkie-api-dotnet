9d0025804c1395dfe02141b890c47fd33b2a3de9
Chus <chusjp@gmail.com>
2019-06-09 10:56:56 +0200

    Reads Json responses as Stream instead of String

--- TikkieAPI/Utilities/JsonResponseParser.cs
+++ TikkieAPI/Utilities/JsonResponseParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using TikkieAPI.Exceptions;
@@ -20,14 +21,19 @@ namespace TikkieAPI.Utilities
         /// <exception cref="TikkieErrorResponseException">If the response message has a non-successful status code.</exception>
         public static async Task<TResponse> GetContentObjectOrExceptionAsync<TResponse>(this HttpResponseMessage response)
         {
-            var contentString = await response.Content.ReadAsStringAsync();
-            if (!response.IsSuccessStatusCode)
+            var contentStream = await response.Content.ReadAsStreamAsync();
+            using (var streamReader = new StreamReader(contentStream))
+            using (var jsonTextReader = new JsonTextReader(streamReader))
             {
-                var errorResponses = JsonConvert.DeserializeObject<ErrorResponses>(contentString);
-                throw new TikkieErrorResponseException($"Server status code: {response.StatusCode}", errorResponses.ErrorResponseArray);
+                var serializer = new JsonSerializer();
+                if (!response.IsSuccessStatusCode)
+                {
+                    var errorResponses = serializer.Deserialize<ErrorResponses>(jsonTextReader);
+                    throw new TikkieErrorResponseException($"Server status code: {response.StatusCode}", errorResponses.ErrorResponseArray);
+                }
+                
+                return serializer.Deserialize<TResponse>(jsonTextReader);
             }
-
-            return JsonConvert.DeserializeObject<TResponse>(contentString);
         }
     }
 }
